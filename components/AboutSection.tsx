0661707fbedc8400443bb4a28b114f4dcc3e4b27
lavymeng <lavymeng@gmail.com>
2023-06-29 19:11:41 +0700

    complete portfolio version 1

---
+++ components/AboutSection.tsx
@@ -0,0 +1,70 @@
+import React from "react";
+import Image from "next/image";
+
+const skills = [
+  { skill: "HTML" },
+  { skill: "CSS" },
+  { skill: "JavaScript" },
+  { skill: "Python" },
+  { skill: "React" },
+  { skill: "Next.js" },
+  { skill: "Tailwind CSS" },
+  { skill: "Git" },
+  { skill: "GitHub" },
+  { skill: "Data Analysis" },
+  { skill: "Research" },
+];
+
+const AboutSection = () => {
+  return (
+    <section id="about">
+      <div className="my-12 pb-12 md:pt-16 md:pb-48">
+        <h1 className="text-center font-bold text-4xl">
+          About Me
+          <hr className="w-6 h-1 mx-auto my-4 bg-teal-500 border-0 rounded"></hr>
+        </h1>
+
+        <div className="flex flex-col space-y-10 items-stretch justify-center align-top md:space-x-10 md:space-y-0 md:p-4 md:flex-row md:text-left">
+          <div className="md:w-1/2 ">
+            <h1 className="text-center text-2xl font-bold mb-6 md:text-left">
+              Get to know me!
+            </h1>
+            <p>Hi, my name is Lavy and I am a software engineer.</p>
+            <br />
+            <p>
+              I studied software engineering at the Kirirom Institute of
+              Technology (KIT) in Cambodia.
+            </p>
+            <br />
+            <p>
+              I love doing yoga and meditation. I also love to read books and
+              learning new things.
+            </p>
+            <br />
+            <p>
+              I believe in hard work. I have a passion for technology,
+              espicially on frontend web. I am always open to new opportunities.
+            </p>
+          </div>
+          <div className="text-center md:w-1/2 md:text-left">
+            <h1 className="text-2xl font-bold mb-6">My Skills</h1>
+            <div className="flex flex-wrap flex-row justify-center z-10 md:justify-start">
+              {skills.map((item, idx) => {
+                return (
+                  <p
+                    key={idx}
+                    className="bg-gray-200 px-4 py-2 mr-2 mt-2 text-gray-500 rounded font-semibold"
+                  >
+                    {item.skill}
+                  </p>
+                );
+              })}
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default AboutSection;
