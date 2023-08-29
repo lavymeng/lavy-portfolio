b5dbf0caf91433473c6d5a6bd0a7edb0adbc0670
lavymeng <lavymeng@gmail.com>
2023-08-29 09:22:39 +0700

    test link

--- components/ProjectsSection.tsx
+++ components/ProjectsSection.tsx
@@ -28,7 +28,7 @@ const projects = [
     image: "/proadvisor.png",
     github:
       "https://github.com/incubation-center/B8-FullStack--Website--Group4",
-    link: "",
+    link: "https://github.com/incubation-center/B8-FullStack--Website--Group4",
   },
   {
     name: "Self-learning",
